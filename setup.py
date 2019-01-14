f06b758b0d739734df6b33a65aff38716a42ae3b
Dave Curtis <dave.n6nz@gmail.com>
2019-01-13 17:25:09 -0800

    minor start on setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,14 @@
+import pathlib
+from setuptools import setup
+
+# Get path to current directory.
+HERE = pathlib.Path(__file__).parent
+		
+# Get the README
+README = (HERE / "README.rst").read_text()
+
+
+
 print("FIXME: No setup.py yet.")
+
 
